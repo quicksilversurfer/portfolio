83ae3b875e87bafbc77c13d3264232a220090b20
Prateek Solanki <prateeksolanki@Prateeks-MacBook-Pro.local>
2022-01-09 23:10:31 -0500

    added case studies link

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -11,8 +11,8 @@ const NavBar = () => {
         <nav className={navBarStyles.nav}>
             <Link className={`${navBarStyles.link}`} to="/">ps</Link>
             <div>
-                {/* <Link className={`${navBarStyles.link} space-right link-style`} to="/blog">blog</Link> */}
-                <a className={`${navBarStyles.link} link-style` } href="https://docs.google.com/document/d/1IVgnbVRpmHoPcfm9noHa2ZWcuguilPSF/edit?usp=sharing&ouid=111091762066531628079&rtpof=true&sd=true" target="_blank" rel="noopener noreferrer">résumé</a>
+                <a className={`${navBarStyles.link} link-style space-right-small`} href="https://prateeksolanki-design.notion.site/Hi-I-m-Prateek-6e5e53483929416e9a05e3f8d213c88a" target="_blank" rel="noopener noreferrer">case studies</a>
+                <a className={`${navBarStyles.link} link-style`} href="https://docs.google.com/document/d/1IVgnbVRpmHoPcfm9noHa2ZWcuguilPSF/edit?usp=sharing&ouid=111091762066531628079&rtpof=true&sd=true" target="_blank" rel="noopener noreferrer">résumé</a>
             </div>
         </nav>
     )
