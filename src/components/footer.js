f7458664978f4ba9f5ecf075400a2460299abac4
Prateek Solanki <prateeksolanki@Prateeks-MacBook-Pro.local>
2022-02-06 13:36:55 -0500

    fix footer links

--- src/components/footer.js
+++ src/components/footer.js
@@ -17,7 +17,7 @@ const Footer = ({ children }) => {
                     <li className={` ${footerStyles.listItem}`}><a href="https://www.linkedin.com/in/prateek-solanki-78313218/" target="_blank" rel="noopener noreferrer" className={` link-style `}>linkedin</a></li>
                     <li className={` ${footerStyles.listItem}`}><a href="https://refind.com/quicksilvrsrfr" target="_blank" rel="noopener noreferrer" className={` link-style `}>refind</a></li>
                 </ul>
-                <p className={`${footerStyles.madeBy}`}>built with <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24"><path d="M12 4.248c-3.148-5.402-12-3.825-12 2.944 0 4.661 5.571 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-6.792-8.875-8.306-12-2.944z"/></svg> using <a href="https://refind.com/quicksilvrsrfr" target="_blank" rel="noopener noreferrer" className={` link-style `}>gatsby.js</a> & <a href="https://refind.com/quicksilvrsrfr" target="_blank" rel="noopener noreferrer" className={` link-style `}>pts.js</a></p>
+                <p className={`${footerStyles.madeBy}`}>built with <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24"><path d="M12 4.248c-3.148-5.402-12-3.825-12 2.944 0 4.661 5.571 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-6.792-8.875-8.306-12-2.944z"/></svg> using <a href="https://www.gatsbyjs.com/" target="_blank" rel="noopener noreferrer" className={` link-style `}>gatsby.js</a> & <a href="https://ptsjs.org/" target="_blank" rel="noopener noreferrer" className={` link-style `}>pts.js</a></p>
             </div>
 
 
